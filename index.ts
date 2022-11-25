cb6721f2a1339521a6c89c01e0a805466fb21907
Florian Herrengt <contact@florianherrengt.com>
2022-11-25 16:14:18 +0000

    rename folder to jsonSchemas

--- index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./types/event.schema";
-export * from "./types/eventsQueueInput.schema";
-import event from "./schemas/event.schema.json";
-import eventsQueueInput from "./schemas/eventsQueueInput.schema.json";
-
-export const schemas = { event, eventsQueueInput };
