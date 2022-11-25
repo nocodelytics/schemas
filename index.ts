0c0434f7a4d2a5aaa002566860c0847f50535827
Florian Herrengt <contact@florianherrengt.com>
2022-11-25 12:00:55 +0000

    export schemas

--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
 export * from "./types/event.schema";
 export * from "./types/eventsQueueInput.schema";
+import event from "./schemas/event.schema.json";
+import eventsQueueInput from "./schemas/eventsQueueInput.schema.json";
+
+export const schemas = { event, eventsQueueInput };
