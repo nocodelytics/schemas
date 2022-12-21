8be8499c3f14f6d269a838536fdfcfdaed7631ac
Florian Herrengt <contact@florianherrengt.com>
2022-12-21 23:45:47 +0100

    switch to 2 go func

--- nocodelytics_schemas.go
+++ nocodelytics_schemas.go
@@ -12,22 +12,22 @@ var eventsQueueInputSchema string
 //go:embed "jsonSchemas/event.schema.json"
 var eventSchema string
 
-type schemas struct {
-	eventsQueueInput *gojsonschema.Schema
-	event            *gojsonschema.Schema
-}
-
-func BuildSchemas() *schemas {
+func BuildEventsQueueInputSchemas() *gojsonschema.Schema {
 	sl := gojsonschema.NewSchemaLoader()
 	eventsQueueInputLoader := gojsonschema.NewStringLoader(eventsQueueInputSchema)
 	eventsQueueInput, err := sl.Compile(eventsQueueInputLoader)
 	if err != nil {
 		panic(err)
 	}
+	return eventsQueueInput
+}
+
+func BuildEventSchemas() *gojsonschema.Schema {
+	sl := gojsonschema.NewSchemaLoader()
 	eventLoader := gojsonschema.NewStringLoader(eventsQueueInputSchema)
 	event, err := sl.Compile(eventLoader)
 	if err != nil {
 		panic(err)
 	}
-	return &schemas{eventsQueueInput, event}
+	return event
 }
